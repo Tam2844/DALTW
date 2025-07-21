c70429114af22461aa159f7a5feb191867d65b35
Tam2844 <tamphan0579@gmail.com>
2025-07-21 15:54:42 +0700

    test2ư

--- DALTW/Areas/Admin/Controllers/CategoryController.cs
+++ DALTW/Areas/Admin/Controllers/CategoryController.cs
@@ -6,8 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace DALTW.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize(Roles = "Admin, Employee")]
+   
     public class CategoryController : Controller
     {
         private readonly ICategoryRepository _repository;
